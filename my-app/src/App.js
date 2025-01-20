c0c5aef5b02c2fb1c805a37a48a806442692419b
nikhilmahato104 <nikhilmahato104@gmail.com>
2025-01-20 09:51:04 +0530

    react-simple-app

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>hi i am a header</p>
+      </header>
+    </div>
+  );
+}
+export default App;                                                  
+
+
+
+
